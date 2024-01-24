dbb42d01424005cc100a21b5640e2a5da03619f7
Cecília Rosa <cecilianrosa.20231@poli.ufrj.br>
2024-01-24 14:15:13 -0300

    restringindo acesso a funcoes de produto

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { VendedorGuard } from './vendedor.guard';
 
 @Controller('produto')
 export class ProdutoController {
   constructor(private readonly produtoService: ProdutoService) {}
 
   @Post()
+  @UseGuards(VendedorGuard)
   create(@Body() createProdutoDto: CreateProdutoDto) {
     return this.produtoService.create(createProdutoDto);
   }
@@ -23,11 +25,13 @@ export class ProdutoController {
   }
 
   @Patch(':id')
+  @UseGuards(VendedorGuard)
   update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
     return this.produtoService.update(+id, updateProdutoDto);
   }
 
   @Delete(':id')
+  @UseGuards(VendedorGuard)
   remove(@Param('id') id: string) {
     return this.produtoService.remove(+id);
   }
--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
+import { VendedorGuard } from './vendedor.guard';
 
 @Module({
   controllers: [ProdutoController],
-  providers: [ProdutoService],
+  providers: [VendedorGuard],
 })
 export class ProdutoModule {}
